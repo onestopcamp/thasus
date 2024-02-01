1d137019cb681417dc660bc87d51062d0d57a37d
Azure <58noah58@gmail.com>
2024-01-31 16:33:13 -0800

    made sure that update_domains was working. added local machine testing code, commented for deployment

--- lambda_deploy.sh
+++ lambda_deploy.sh
@@ -2,16 +2,15 @@
 echo "Build and Deploy Thasus Lambda Function to AWS"
 
 echo "Packaging"
-cd /Users/farhan.syed/farhan/scraper/thasus/
 rm -rf thasus.zip
-zip -r thasus.zip . -x "./.idea/*" "./python/*" "./thasus-lib.zip" "./latest_run.log" "./.gitignore" "./.git/*"
+zip -r thasus.zip . -x "./.idea/*" "./python/*" "./thasus-lib.zip" "./latest_run.log" "./.gitignore" "./.git/*" "./venv/*"
 echo "Packaging complete"
 echo "Deploying Thasus to AWS"
-aws lambda update-function-code --function-name thasus --zip-file fileb:///Users/farhan.syed/farhan/scraper/thasus/thasus.zip --publish --profile cadmus_cli
+aws lambda update-function-code --function-name thasus --zip-file fileb:///mnt/e/IntelliJ/thasus/thasus.zip --publish --profile thasus_cli --region us-west-2
 echo "Thasus deployment complete"
 echo "Running Thasus in Lambda"
 sleep 5
-aws lambda invoke --profile cadmus_cli --function-name thasus --payload fileb://test_event.json latest_run.log
+aws lambda invoke --profile thasus_cli --function-name thasus --payload fileb://test_event.json latest_run.log --region us-west-2
 cat latest_run.log
 echo ""
 echo "Done"
